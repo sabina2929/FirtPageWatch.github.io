c5fefad7f44bb6b81ff8d1f52343a0127851efb7
SabinaBatyrbek <75356847+SabinaBatyrbek@users.noreply.github.com>
2022-11-04 14:22:32 +0600

    First Commit
    First Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.rev').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+
+})
+
+$(function(){
+    $('.rev1').slick({
+        dots: true,
+      });
+
+})
